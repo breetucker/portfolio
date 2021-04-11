0f6e29386374a48c80f7ad9a635bff8cfeda1068
Bree <46540002+breetucker@users.noreply.github.com>
2021-04-11 00:31:24 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
   <footer>
-		<p class="far fa-copyright colorChange" style="color:var(--darkPink);"> Bree Tucker</i></p>
+		<p class="far fa-copyright colorChange" style="color:var(--darkPink);">Copyright 2021 Bree Tucker</i></p>
 	</footer>
